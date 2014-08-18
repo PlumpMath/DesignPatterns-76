1f9c0eff4223f6e52255207ee4238301b06092da
jjokela <jarmo.jokela@gmail.com>
2014-08-18 10:03:41 +0300

    Added Singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        // Thread safe Singleton with fully lazy instantiation á la Jon Skeet:
+        // http://csharpindepth.com/Articles/General/Singleton.aspx
+        Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested
+        {
+            // Explicit static constructor to tell C# compiler
+            // not to mark type as beforefieldinit
+            static Nested()
+            {
+            }
+
+            internal static readonly Singleton instance = new Singleton();
+        }
+    }
+}
